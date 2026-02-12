b3cbc9ecaaaf7fa7d6829caee93fae6bd1844ad3
Isaac Keenan <peacoltrain@gmail.com>
2026-02-12 16:43:21 -0700

    Got about content ported over

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -2,8 +2,18 @@ import React from 'react';
 
 export function About() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>about displayed here</div>
+    <main>
+        <br/>
+        <p>This is a app that allows users to take a little 
+            break from their day and maintain a healthy and 
+            flexible mind.
+        </p>
+        <p>Any recommendations or issues can be reported to me through the repository</p>
+
+        <p>Please enjoy this lovely image of the ocean</p>
+
+        <img src="../../public/oceanview.jpg" alt="Bright Blue Ocean" width="600"></img>
+        
     </main>
   );
 }
