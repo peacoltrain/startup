72b31d53c5d34367b5170d519c1bd2b430c7bdf9
Isaac Keenan <peacoltrain@gmail.com>
2026-02-12 16:29:49 -0700

    Finished debugging route connections

--- src/app.jsx
+++ src/app.jsx
@@ -1,33 +1,43 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { Game } from './game/game';
+import { Scores } from './scores/scores';
+import { About } from './about/about';
+
 
 export default function App() {
   return (
-    <div>
+    <BrowserRouter>
         <header>
         <nav className="navbar fixed-top navbar-dark">
-            <a className="navbar-brand" href="#">Memory Master</a>
+            <div className="navbar-brand">Memory Master</div>
             <menu className="navbar-nav">
             <li className="nav-item">
-                <a className="nav-link active" href="index.html">Home</a>
+                <NavLink className="nav-link" to="">Login</NavLink>
             </li>
             <li className="nav-item">
-                <a className="nav-link" href="game.html">Play</a>
+                <NavLink className="nav-link" to="game">Play</NavLink>
             </li>
             <li className="nav-item">
-                <a className="nav-link" href="scores.html">Scores</a>
+                <NavLink className="nav-link" to="scores">Scores</NavLink>
             </li>
             <li className="nav-item">
-                <a className="nav-link" href="about.html">About</a>
+                <NavLink className="nav-link" to="about">About</NavLink>
             </li>
             </menu>
         </nav>
         </header>
 
-        <main>
-        <p>This is placeholder for main</p>
-        </main>
+        <Routes>
+            <Route path='/' element={<Login />} exact />
+            <Route path='/game' element={<Game />} />
+            <Route path='/scores' element={<Scores />} />
+            <Route path='/about' element={<About />} />
+            <Route path='*' element={<NotFound />} />
+        </Routes>
 
         <footer>
         <hr />
@@ -35,6 +45,10 @@ export default function App() {
         <br />
         <a href="https://github.com/peacoltrain/startup.git">Memory Github</a>
         </footer>
-    </div>
+    </BrowserRouter>
   );
+}
+
+function NotFound() {
+  return <main className="container-fluid bg-secondary text-center">404: Return to sender. Address unknown.</main>;
 }
