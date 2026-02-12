7832434d9bd826d716001bba646c79732df2aec0
Isaac Keenan <peacoltrain@gmail.com>
2026-02-12 16:39:43 -0700

    Finished Game page

--- src/game/game.jsx
+++ src/game/game.jsx
@@ -1,9 +1,167 @@
 import React from 'react';
+import './game.css'
 
 export function Game() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>Game displayed here</div>
+    <main>
+      <section className="sidebar">
+        <div className="players">
+          Player:
+          <span className="player-name">Player Name</span>
+        </div>
+        <div className="turns">
+          Turn:
+          <span className="player-turns">13</span>
+        </div>
+        <br />
+        <div className="websocket">Notifications:
+        <p>Jace has requested to play a game with you.</p></div>
+      </section>
+      <section className="gameboard">
+        <table>
+          <tr>
+            <td>
+              <button>
+                MM
+              </button>
+            </td>
+            <td>
+              <button>
+                MM
+              </button>
+            </td>
+            <td>
+              <button>
+                MM
+              </button>
+            </td>
+            <td>
+              <button>
+                MM
+              </button>
+            </td>
+            <td>
+              <button>
+                MM
+              </button>
+            </td>
+          </tr>
+                    <tr>
+            <td>
+              <button>
+                MM
+              </button>
+            </td>
+            <td>
+              <button>
+                MM
+              </button>
+            </td>
+            <td>
+              <button>
+                MM
+              </button>
+            </td>
+            <td>
+              <button>
+                MM
+              </button>
+            </td>
+            <td>
+              <button>
+                MM
+              </button>
+            </td>
+          </tr>
+                    <tr>
+            <td>
+              <button>
+                MM
+              </button>
+            </td>
+            <td>
+              <button>
+                MM
+              </button>
+            </td>
+            <td>
+              <button>
+                MM
+              </button>
+            </td>
+            <td>
+              <button>
+                MM
+              </button>
+            </td>
+            <td>
+              <button>
+                MM
+              </button>
+            </td>
+          </tr>
+                    <tr>
+            <td>
+              <button>
+                MM
+              </button>
+            </td>
+            <td>
+              <button>
+                MM
+              </button>
+            </td>
+            <td>
+              <button>
+                MM
+              </button>
+            </td>
+            <td>
+              <button>
+                MM
+              </button>
+            </td>
+            <td>
+              <button>
+                MM
+              </button>
+            </td>
+          </tr>
+                    <tr>
+            <td>
+              <button>
+                MM
+              </button>
+            </td>
+            <td>
+              <button>
+                MM
+              </button>
+            </td>
+            <td>
+              <button>
+                MM
+              </button>
+            </td>
+            <td>
+              <button>
+                MM
+              </button>
+            </td>
+            <td>
+              <button>
+                MM
+              </button>
+            </td>
+          </tr>
+        </table>
+        <div>
+        <button className="newGame">New Game</button>
+        </div>
+      </section>
+      <section className="apiplace">
+        <div>This is where I will have a word of the day api generated so we can learn something new</div>
+      </section>
     </main>
   );
 }
