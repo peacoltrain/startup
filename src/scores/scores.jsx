8ed1dd8ccb7589a92b68a60b14a0128e8da121ea
Isaac Keenan <peacoltrain@gmail.com>
2026-02-12 16:33:17 -0700

    Transfered over Scores

--- src/scores/scores.jsx
+++ src/scores/scores.jsx
@@ -1,9 +1,39 @@
 import React from 'react';
+import './scores.css'
 
 export function Scores() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>Scores displayed here</div>
+    <main>
+      <table>
+        <thead>
+          <tr>
+            <th></th>
+            <th>Name</th>
+            <th>Turns</th>
+            <th>Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Jeff Smith</td>
+            <td>34</td>
+            <td>Jan 27, 2026</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Angela Jenkins</td>
+            <td>20</td>
+            <td>Feb 3, 2026</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>Maddox Cook</td>
+            <td>52</td>
+            <td>Mar 23, 2026</td>
+          </tr>
+        </tbody>
+      </table>
     </main>
   );
 }
