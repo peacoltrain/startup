983ea5fd39941dc399411b2e1f2cf2a946630079
Isaac Keenan <peacoltrain@gmail.com>
2026-02-23 07:30:26 -0700

    Roughed out some basics with login page and got username to be saved locally

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,19 +1,31 @@
 import React from 'react';
 
 export function Login() {
+  const [username, setUsername] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  function loginUser(){
+    console.log("Tried to log in: " + username);
+    localStorage.setItem('user', username)
+  }
+
+  function userChange(e){
+    setUsername(e.target.value)
+  }
+
   return (
     <main>
       <h1>Try to be a Memory Master</h1>
-      <form method="get" action="game.html">
+      <form onSubmit={loginUser}>
         <div>
           <span>Username</span>
-          <input id="username" type="text" placeholder="Username" autoComplete="username" />
+          <input id="username" type="text" placeholder="Username" autoComplete="username" onChange={userChange}/>
         </div>
         <div>
           <span>Password</span>
           <input id="password" type="password" placeholder="Password" />
         </div>
-        <button type="submit">Login</button>
+        <button type="submit"  onClick={loginUser}>Login</button>
         <button type="submit">Sign UP</button>
       </form>
     </main>
