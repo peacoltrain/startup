35b013ddc7e8f569d884731f48554fbed1dc39b7
Isaac Keenan <peacoltrain@gmail.com>
2026-02-22 17:08:18 -0700

    Added React functionality to about page. Doesn't do anything currently. I have decided to move word of the date to the about page.

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -1,6 +1,21 @@
 import React from 'react';
 
 export function About() {
+  const [image, setImage] = React.useState('This is a placeholder for when I will have the image be called.')
+  const [word, setWord] = React.useState('I have decided that I am going to do the word of the Day on the about page.')
+  const [def, setDefinition] = React.useState('Definition Placeholder')
+
+  //I also thing I would be more efficient to save the word of the day and definition on the backend
+  //Cross that bridge when i get to it.
+
+  React.useEffect(
+    () => {
+      setImage('oceanview.jpg');
+      setWord("Foray");
+      setDefinition("A foray is an initial and often hesitant attempt to do something in a new or different field or area of activity");
+    }, []    
+  );
+
   return (
     <main>
         <br/>
@@ -12,7 +27,10 @@ export function About() {
 
         <p>Please enjoy this lovely image of the ocean</p>
 
-        <img src="/oceanview.jpg" alt="Bright Blue Ocean" width="600"></img>
+        <img src={image} alt="Bright Blue Ocean" width="600"></img>
+
+        <div>The word of the day will be {word}:</div>
+        <div>Definition: {def}</div>
         
     </main>
   );
