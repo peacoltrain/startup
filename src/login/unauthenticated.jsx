437d40f9ee0d5fab5ef709ec84482b3b141fbad7
Isaac Keenan <peacoltrain@gmail.com>
2026-02-23 18:28:15 -0700

    Got various login view depending on current auth standing. Added login and sign up functions

--- src/login/unauthenticated.jsx
+++ src/login/unauthenticated.jsx
@@ -2,28 +2,32 @@ import React from 'react'
 
 
 export function Unauthenticated(props) {
-    const [username, setUsername] = React.useState('');
+    const [userName, setUsername] = React.useState('');
     const [password, setPassword] = React.useState('');
 
     function loginUser(){
-        console.log("Tried to log in: " + username);
-        localStorage.setItem('user', username)
+        console.log("Tried to sign up " + userName + " with a password of " + password)
+        localStorage.setItem('userName', userName);
     }
 
+    function signUp(){
+        console.log("Tried to sign up " + userName + " with a password of " + password);
+        localStorage.setItem('userName', userName);
+    }
 
     return (
         <form>
         <div>This is a test!</div>
         <div>
           <span>Username</span>
-          <input id="username" type="text" placeholder="Username" autoComplete="username" onChange={userChange}/>
+          <input id="userName" type="text" placeholder="Username" autoComplete="userName" onChange={(e) => setUsername(e.target.value)}/>
         </div>
         <div>
           <span>Password</span>
-          <input id="password" type="password" placeholder="Password" />
+          <input id="password" type="password" placeholder="Password" onChange={(e) => setUsername(e.target.value)}/>
         </div>
         <button type="submit"  onClick={loginUser}>Login</button>
-        <button type="submit">Sign UP</button>
+        <button type="submit" onClick={signUp}>Sign UP</button>
       </form>
     );
 }
