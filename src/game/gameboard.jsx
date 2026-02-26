b1845aef755e512b99cebe603e973eb46fa0596f
Isaac Keenan <peacoltrain@gmail.com>
2026-02-25 22:08:22 -0700

    Got card to flip.

--- src/game/gameboard.jsx
+++ src/game/gameboard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export function GameBoard(props) {
     const userName = props.userName;
     const [score, setScore] = React.useState(null)
-    const [cards, setCards] = React.useState([]);
+    const [cards, setCards] = React.useState(placeCards([null, null, null, null, null, null, null, null, null, null]));
     const [inGame, setInGame] = React.useState(false);
 
     {/* This is where I shuffle my array
@@ -22,8 +22,7 @@ export function GameBoard(props) {
         return array;
     }
 
-    function placeCards() {
-        const values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    function placeCards(values) {
         const allValues = [...values, ...values];
 
         let shuffled = shuffle(allValues);
@@ -36,10 +35,20 @@ export function GameBoard(props) {
         return cardSet;
     }
 
+    function cardFlip(cardId) {
+        setCards(prevCards =>
+            prevCards.map(card =>
+                card.id === cardId
+                    ? { ...card, flipped: true }
+                    : card
+            )
+        );
+    }
+
     function newGame() {
         setScore(0)
         setInGame(true)
-        setCards(() => placeCards())
+        setCards(() => placeCards([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
     }
 
     return (
@@ -49,39 +58,38 @@ export function GameBoard(props) {
         <table>
           <tbody>
           <tr>
-            <td><button disabled={!inGame}>MM</button></td>
-            <td><button disabled={!inGame}>MM</button></td>
-            <td><button disabled={!inGame}>MM</button></td>
-            <td><button disabled={!inGame}>MM</button></td>
-            <td><button disabled={!inGame}>MM</button></td>
+            <td><button disabled={!inGame} onClick={() => cardFlip(0)}>{cards[0].flipped ? cards[0].value : 'MM'}</button></td>
+            <td><button disabled={!inGame} onClick={() => cardFlip(1)}>MM</button></td>
+            <td><button disabled={!inGame} onClick={() => cardFlip(2)}>MM</button></td>
+            <td><button disabled={!inGame} onClick={() => cardFlip(3)}>MM</button></td>
+            <td><button disabled={!inGame} onClick={() => cardFlip(4)}>MM</button></td>
           </tr>
           <tr>
-            <td><button disabled={!inGame}>MM</button></td>
-            <td><button disabled={!inGame}>MM</button></td>
-            <td><button disabled={!inGame}>MM</button></td>
-            <td><button disabled={!inGame}>MM</button></td>
-            <td><button disabled={!inGame}>MM</button></td>
+            <td><button disabled={!inGame} onClick={() => cardFlip(5)}>MM</button></td>
+            <td><button disabled={!inGame} onClick={() => cardFlip(6)}>MM</button></td>
+            <td><button disabled={!inGame} onClick={() => cardFlip(7)}>MM</button></td>
+            <td><button disabled={!inGame} onClick={() => cardFlip(8)}>MM</button></td>
+            <td><button disabled={!inGame} onClick={() => cardFlip(9)}>MM</button></td>
           </tr>
           <tr>
-            <td><button disabled={!inGame}>MM</button></td>
-            <td><button disabled={!inGame}>MM</button></td>
-            <td><button disabled={!inGame}>MM</button></td>
-            <td><button disabled={!inGame}>MM</button></td>
-            <td><button disabled={!inGame}>MM</button></td>
+            <td><button disabled={!inGame} onClick={() => cardFlip(10)}>MM</button></td>
+            <td><button disabled={!inGame} onClick={() => cardFlip(11)}>MM</button></td>
+            <td><button disabled={!inGame} onClick={() => cardFlip(12)}>MM</button></td>
+            <td><button disabled={!inGame} onClick={() => cardFlip(13)}>MM</button></td>
+            <td><button disabled={!inGame} onClick={() => cardFlip(14)}>MM</button></td>
           </tr>
           <tr>
-            <td><button disabled={!inGame}>MM</button></td>
-            <td><button disabled={!inGame}>MM</button></td>
-            <td><button disabled={!inGame}>MM</button></td>
-            <td><button disabled={!inGame}>MM</button></td>
-            <td><button disabled={!inGame}>MM</button></td>
+            <td><button disabled={!inGame} onClick={() => cardFlip(15)}>MM</button></td>
+            <td><button disabled={!inGame} onClick={() => cardFlip(16)}>MM</button></td>
+            <td><button disabled={!inGame} onClick={() => cardFlip(17)}>MM</button></td>
+            <td><button disabled={!inGame} onClick={() => cardFlip(18)}>MM</button></td>
+            <td><button disabled={!inGame} onClick={() => cardFlip(19)}>MM</button></td>
           </tr>
           </tbody>
         </table>
         <div>
         <button className="newGame" onClick={newGame}>New Game</button>
         </div>
-        {console.log(cards)}
       </section>
     ); 
 }
