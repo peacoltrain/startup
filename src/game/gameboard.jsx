f1027071438b7dac72137a5c2d1e7f28df79e4a3
Isaac Keenan <peacoltrain@gmail.com>
2026-02-25 20:41:10 -0700

    Created a shuffle and placeCards functions. Used Fisher-Yates as my research shows that javascript sometimes struggles with randomness.

--- src/game/gameboard.jsx
+++ src/game/gameboard.jsx
@@ -3,7 +3,38 @@ import React from 'react'
 export function GameBoard(props) {
     const userName = props.userName;
     const [score, setScore] = React.useState(null)
+    const [cards, setCards] = React.useState([]);
 
+    {/* This is where I shuffle my array
+        I am using the Fisher-Yates shuffle.
+        I hope it helps remove some interal bias */}
+    function shuffle(array) {
+        let i = array.length;
+        let r;
+
+        while (i !== 0) {
+            r = Math.floor(Math.randome() * i);
+            i--;
+
+            [array[i], array[r] = [array[r], array[i]]];
+        }
+
+        return array;
+    }
+
+    function placeCards() {
+        const values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        const allValues = [...values, ...values];
+
+        let shuffled = shuffle(allValues);
+        cardSet = shuffled.map((value, index) => ({
+            id: index,
+            value: value,
+            flipped: false,
+            matched: false
+        }));
+        return cardSet;
+    }
 
     return (
         <section className="gameboard">
