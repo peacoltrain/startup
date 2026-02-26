48330d10d5f2dc9f3e3240638c40fc412512d662
Isaac Keenan <peacoltrain@gmail.com>
2026-02-25 23:19:38 -0700

    Got backbone of game. Need to code end state.

--- src/game/gameboard.jsx
+++ src/game/gameboard.jsx
@@ -4,7 +4,7 @@ export function GameBoard(props) {
     const userName = props.userName;
     const [score, setScore] = React.useState(null)
     const [cards, setCards] = React.useState(placeCards([null, null, null, null, null, null, null, null, null, null]));
-    const [inGame, setInGame] = React.useState(false);
+    const [locked, setLock] = React.useState(true);
     const [firstFlipp, setFirstFlipp] = React.useState(null);
 
     {/* This is where I shuffle my array
@@ -40,19 +40,33 @@ export function GameBoard(props) {
         console.log("Match successfully loaded!")
         {console.log(firstFlipp)}
         {console.log(flippedCard)}
-        /*If the cards are a match increase score by one, clear first and second card.*/
-        if (firstFlipp.value === flippedCard.value){
-            console.log("Matchfound!");
-            setFirstFlipp(null);
-            setScore(oldscore => oldscore++);
-            return;
-        }
 
-        /*If not match, make cards unflipped, clear first and second card, increase score by one*/
-        console.log("No Match");
-        flipCard([firstFlipp.id, flippedCard.id]);
-        setFirstFlipp(null);
-        setScore(oldscore => oldscore++);
+        setTimeout(() => {
+            /*If the cards are a match increase score by one, clear first and second card.*/
+            if (firstFlipp.value === flippedCard.value){
+                console.log("Matchfound!");
+                matchCard([firstFlipp.id, flippedCard.id])
+                setFirstFlipp(null);
+                setScore(oldScore => oldScore +1);
+                return;
+            } else { /*If not match, make cards unflipped, clear first and second card, increase score by one*/
+                console.log("No Match");
+                flipCard([firstFlipp.id, flippedCard.id]);
+                setFirstFlipp(null);
+                setScore(oldScore => oldScore +1);
+            }
+
+            setLock(false);
+        }, 3000);
+        
+    }
+
+    function matchCard(matchedIDs) {
+        setCards(prevCards =>
+            prevCards.map(card =>
+                matchedIDs.includes(card.id) ? { ...card, matched: true} : card
+            )
+        );
     }
 
     function flipCard(flippedCardsIDs) {
@@ -66,17 +80,19 @@ export function GameBoard(props) {
     function pick(flippedCard) {
         flipCard([flippedCard.id])
 
+
         if (firstFlipp === null) {
             setFirstFlipp(flippedCard);
             return;
         }
 
+        setLock(true)
         match(flippedCard);
     }
 
     function newGame() {
         setScore(0)
-        setInGame(true)
+        setLock(false)
         setCards(() => placeCards([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
     }
 
@@ -91,7 +107,7 @@ export function GameBoard(props) {
                     {cards.slice(row * 5,(row * 5) + 5).map(card => (
                         <td>
                             <button
-                            disabled={!inGame}
+                            disabled={locked || card.matched || card.flipped}
                             onClick={() => pick(card)}
                             >
                             {card.flipped ? card.value : 'MM'}
