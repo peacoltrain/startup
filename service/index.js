f53c664917c885944e6aa60eb802a35ab920a37b
Isaac Keenan <peacoltrain@gmail.com>
2026-03-11 15:16:12 -0600

    Added create and loging api

--- service/index.js
+++ service/index.js
@@ -1,19 +1,44 @@
 const express = require('express');
 const app = express();
-
 const cookieParser = require('cookie-parser');
 const bcrypt = require('bcryptjs');
 const uuid = require('uuid');
 
+const authCookieName = 'token';
+let users = [];
+let scores = [];
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
 app.use(express.json());
-
-let users = [];
-let scores = [];
+app.use(cookieParser());
+app.use(express.static('public'));
 
 let apiRouter = express.Router();
 app.use('/api', apiRouter);
+
+apiRouter.post('/auth/create', async (req, res) => {
+    if (await findUser('username', req.body.username)) {
+        res.status(409).send({ msg: "User exists already"});
+    } else {
+        const user = await createUser(req.body.username, req.body.password);
+
+        setAuthCookie(res, user.token);
+        res.send({ username: user.username});
+    }
+});
+
+apiRouter.post('/auth/login', async (req, res) => {
+    const user = await findUser('username', req.body.username);
+    if (user) {
+        if (await bcrypt.compare(req.body.password, user.password)) {
+            user.tocken = uuid.v4();
+            setAuthCookie(res, user.token);
+            res.send({ username: user.username});
+            return;
+        }
+    }
+    res.status(401).send({ msg: 'Unauthorized'});
+});
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
