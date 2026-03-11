a94a3804b5429e0fe31cea7ffdaa9961237940ee
Isaac Keenan <peacoltrain@gmail.com>
2026-03-11 14:27:52 -0600

    Added packages and some scaffolding

--- service/index.js
+++ service/index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const app = express();
 
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcryptjs');
+const uuid = require('uuid');
+
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
-app.use((req, res) => {
-    res.send({msg: 'Startup service is functional'});
-});
+app.use(express.json());
+
+let users = [];
+let scores = [];
+
+let apiRouter = express.Router();
+app.use('/api', apiRouter);
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
